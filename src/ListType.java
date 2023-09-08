5d8e5fe82aa59e232a98c160468229182517cc2a
sky1231234 <blueskyee52@gmail.com>
2023-09-08 17:46:17 +0900

    LIST 참조 타입 불변 객체 Example

--- src/ListType.java
+++ src/ListType.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class ListType {
@@ -6,9 +8,13 @@ public class ListType {
     private final List<Age> list;
 
     public ListType(final List<Age> list){
+//그대로 참조하지 않고, 새로운 list 객체를 만들어 사용
         this.list = new ArrayList<>(list);
     }
+
+
     public List<Age> getList() {
-        return list;
+//        값 변경 불가능하도록 unmodifiableList() 메소드 사용
+        return Collections.unmodifiableList(list);
     }
 }
